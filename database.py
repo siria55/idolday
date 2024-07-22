f5d9cd19bcc8d99d7bc76e11cc065feb821713fc
Siria <siria55lee@gmail.com>
2024-07-23 00:17:33 +0800

    update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://hotdog:Zzxscdrf2323@rm-2ze8try6287fyk6j3go.mysql.rds.aliyuncs.com:3306/bothub'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://hotdog:TOAItoai1234@rm-2ze8try6287fyk6j3go.mysql.rds.aliyuncs.com:3306/bothub'
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
