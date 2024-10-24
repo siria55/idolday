c23a44b788728911445c184b6ffc5757e8196658
siria <siria55lee@gmail.com>
2024-10-24 12:04:25 +0800

    expose admin router

--- api/routers.py
+++ api/routers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from .endpoints import xox
+from .admin import xox as admin_xox
 
 api_router = APIRouter()
 api_router.include_router(xox.router, tags=["XOX"])
+
+api_router.include_router(admin_xox.router, tags=["Admin"])
